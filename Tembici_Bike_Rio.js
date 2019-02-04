03cab193bfa469055483522da87a54b34ed69328
Eduardo <eduardo_felipe_oliveira@yahoo.com.br>
2019-02-04 13:09:08 -0200

    Tudo para espera

--- Tembici_Bike_Rio.js
+++ Tembici_Bike_Rio.js
@@ -4,31 +4,29 @@ var esl_server = new esl.Server({host: '0.0.0.0', port: 8085, myevents:true}, fu
     console.log("Gerenciador de fila iniciado")
 })
 
-const espera = []
-//const ativa = []
+let tempo = 0
 
 esl_server.on('connection::ready', function(conn, id) {
-    //console.log(conn.channelData.headers)
+    tempo = tempo + 20000
     console.log('Chamada com ID: ' + id + ' pronta para manipulação')
-
-    /*if(ativa.length < 5){
-        ativa.push([id, conn])
-
-        conn.execute('set', 'effective_caller_id_name=${caller_id_number}Bike_Rio',  cb => {
-            conn.execute('set', 'effective_caller_id_number=${caller_id_number}Bike_Rio', cb => {
-                conn.execute('set', 'bridge_generate_comfort_noise=true', cb => {
-                    conn.execute('bridge', `sofia/gateway/gateway_cloud/551140036054`, cb => {
-                        conn.execute('hangup', cb => {})
+    //console.log(conn.channelData.headers)
+    
+    conn.execute('answer', cb => {
+        conn.execute('valet_park', 'valet_lot $1', cb => {})
+        
+        setInterval(() => {
+            conn.execute('set', 'effective_caller_id_name=${caller_id_number}Bike_Rio',  cb => {
+                conn.execute('set', 'effective_caller_id_number=${caller_id_number}Bike_Rio', cb => {
+                    conn.execute('set', 'bridge_generate_comfort_noise=true', cb => {
+                        conn.execute('bridge', `sofia/gateway/gateway_cloud/551140036054`, cb => {
+                            tempo = tempo - 20000
+                            conn.execute('hangup', cb => {})
+                        })
                     })
                 })
             })
-        })
-    }else{*/
-        conn.execute('answer', cb => {
-            espera.push(conn)
-            conn.execute('valet_park', 'valet_lot $1', cb => {})
-        })
-    //}
+        }, tempo)
+    })
 })
 
 esl_server.on('connection::open', (conn, id) => {
@@ -37,30 +35,4 @@ esl_server.on('connection::open', (conn, id) => {
 
 esl_server.on('connection::close', (conn, id) => {
     console.log('Chamada com ID: ' + id + ' encerrada')
-    /*ativa.find((element, index) => {
-        if(element[0] === id){
-            ativa.splice(index)s
-        }
-    })*/
 })
-
-setInterval(() => {
-    if(espera.length > 0){
-        let atual = espera.shift()
-        
-        atual.execute('set', 'effective_caller_id_name=${caller_id_number}Bike_Rio',  cb => {
-            atual.execute('set', 'effective_caller_id_number=${caller_id_number}Bike_Rio', cb => {
-                atual.execute('set', 'bridge_generate_comfort_noise=true', cb => {
-                    atual.execute('bridge', `sofia/gateway/gateway_cloud/551140036054`, cb => {
-                        atual.execute('hangup', cb => {})
-                    })
-                })
-            })
-        })
-    }
-
-}, 20000)
-
-setInterval(() => {
-    console.log(`Espera: ${espera.length}`)
-}, 5000)
