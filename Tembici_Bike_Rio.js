31c3a85fc30bb3c2a04373f34e0d76c1b3d81941
Eduardo <eduardo_felipe_oliveira@yahoo.com.br>
2019-02-04 13:24:22 -0200

    Tudo para espera

--- Tembici_Bike_Rio.js
+++ Tembici_Bike_Rio.js
@@ -4,34 +4,29 @@ var esl_server = new esl.Server({host: '0.0.0.0', port: 8085, myevents:true}, fu
     console.log("Gerenciador de fila iniciado")
 })
 
-//let tempo = 0
-//let park = 1
+let tempo = 20000
+let park = 1
 
 esl_server.on('connection::ready', function(conn, id) {
-    //tempo = tempo + 20000
     console.log('Chamada com ID: ' + id + ' pronta para manipulação')
     //console.log(conn.channelData.headers)
     
-    conn.execute('answer', cb => {
-        //conn.execute('valet_park', `valet_lot ${park}`, cb => {
-        //    park = park + 1
-        //})
+    conn.execute('answer', function(cb) {
+        conn.execute('valet_park', `valet_lot ${park}`, cb => {
+            park = park + 1
 
-        //conn.execute('playback', `/usr/share/freeswitch/sounds/music/default/8000/danza-espanola-op-37-h-142-xii-arabesca.wav`, cb => {})
-
-        //setTimeout(() => {
-            conn.execute('set', 'effective_caller_id_name=${caller_id_number}Bike_Rio',  cb => {
-                conn.execute('set', 'effective_caller_id_number=${caller_id_number}Bike_Rio', cb => {
-                    conn.execute('set', 'bridge_generate_comfort_noise=true', cb => {
-                        //tempo = tempo - 20000
-                        conn.execute('bridge', `sofia/gateway/gateway_cloud/551140036054`, cb => {
-                            conn.execute('hangup', cb => {})
+            setTimeout(() => {
+                conn.execute('set', 'effective_caller_id_name=${caller_id_number}Bike_Rio',  function(cb) {
+                    conn.execute('set', 'effective_caller_id_number=${caller_id_number}Bike_Rio', function(cb) {
+                        conn.execute('set', 'bridge_generate_comfort_noise=true', function(cb) {
+                            conn.execute('bridge', `sofia/gateway/gateway_cloud/551140036054`, function(cb) {
+                                conn.execute('hangup', function(cb) {})
+                            })
                         })
                     })
                 })
-            })
-        //}, tempo)
-        //console.log('Tempo de espera da chamada: ' + tempo)
+            }, tempo)
+        })
     })
 })
 
