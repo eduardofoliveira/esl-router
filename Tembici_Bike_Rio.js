2e4c4f7eac00e9c5acfdd64d70089e0cc32f312c
Eduardo <eduardo_felipe_oliveira@yahoo.com.br>
2019-02-04 13:42:46 -0200

    Tudo para espera

--- Tembici_Bike_Rio.js
+++ Tembici_Bike_Rio.js
@@ -14,21 +14,20 @@ esl_server.on('connection::ready', function(conn, id) {
     conn.execute('answer', function(cb) {
         //conn.execute('valet_park', `valet_lot ${park}`, cb => {
             //park = park + 1
-        conn.execute('playback', 'local_stream://moh', cb => {
-            console.log('cb do playback')
+        conn.execute('playback', 'local_stream://moh', cb => {})
 
-            setTimeout(() => {
-                conn.execute('set', 'effective_caller_id_name=${caller_id_number}Bike_Rio',  function(cb) {
-                    conn.execute('set', 'effective_caller_id_number=${caller_id_number}Bike_Rio', function(cb) {
-                        conn.execute('set', 'bridge_generate_comfort_noise=true', function(cb) {
-                            conn.execute('bridge', `sofia/gateway/gateway_cloud/551140036054`, function(cb) {
-                                conn.execute('hangup', function(cb) {})
-                            })
+        setTimeout(() => {
+            conn.execute('set', 'effective_caller_id_name=${caller_id_number}Bike_Rio',  function(cb) {
+                conn.execute('set', 'effective_caller_id_number=${caller_id_number}Bike_Rio', function(cb) {
+                    conn.execute('set', 'bridge_generate_comfort_noise=true', function(cb) {
+                        conn.execute('bridge', `sofia/gateway/gateway_cloud/551140036054`, function(cb) {
+                            conn.execute('hangup', function(cb) {})
                         })
                     })
                 })
-            }, tempo)
-        })
+            })
+        }, tempo)
+        
     })
 })
 
