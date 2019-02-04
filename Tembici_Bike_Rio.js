dd47924491f6073856ffa9ad59a2ac4b37435d4e
Eduardo <eduardo_felipe_oliveira@yahoo.com.br>
2019-02-04 13:46:23 -0200

    Tudo para espera

--- Tembici_Bike_Rio.js
+++ Tembici_Bike_Rio.js
@@ -5,31 +5,31 @@ var esl_server = new esl.Server({host: '0.0.0.0', port: 8085, myevents:true}, fu
 })
 
 let tempo = 20000
-//let park = 8000
+const lista = []
 
 esl_server.on('connection::ready', function(conn, id) {
     console.log('Chamada com ID: ' + id + ' pronta para manipulação')
     //console.log(conn.channelData.headers)
     
     conn.execute('answer', function(cb) {
-        //conn.execute('valet_park', `valet_lot ${park}`, cb => {
-            //park = park + 1
         conn.execute('playback', 'local_stream://moh', cb => {})
-
-        setTimeout(() => {
-            conn.execute('set', 'effective_caller_id_name=${caller_id_number}Bike_Rio',  function(cb) {
-                conn.execute('set', 'effective_caller_id_number=${caller_id_number}Bike_Rio', function(cb) {
-                    conn.execute('set', 'bridge_generate_comfort_noise=true', function(cb) {
-                        conn.execute('bridge', `sofia/gateway/gateway_cloud/551140036054`, function(cb) {
-                            conn.execute('hangup', function(cb) {})
-                        })
-                    })
-                })
-            })
-        }, tempo)
-        
+        lista.push(conn)
     })
 })
+
+setInterval(() => {
+    let conn = lista.shift()
+
+    conn.execute('set', 'effective_caller_id_name=${caller_id_number}Bike_Rio',  function(cb) {
+        conn.execute('set', 'effective_caller_id_number=${caller_id_number}Bike_Rio', function(cb) {
+            conn.execute('set', 'bridge_generate_comfort_noise=true', function(cb) {
+                conn.execute('bridge', `sofia/gateway/gateway_cloud/551140036054`, function(cb) {
+                    conn.execute('hangup', function(cb) {})
+                })
+            })
+        })
+    })
+}, tempo)
 
 esl_server.on('connection::open', (conn, id) => {
 	console.log('Chamada com ID: ' + id + ' chegou')
