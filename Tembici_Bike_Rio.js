6a030a6bd20d2f29664266e7cca909de6f824c3d
Eduardo <eduardo_felipe_oliveira@yahoo.com.br>
2019-02-04 11:22:01 -0200

    Correção

--- Tembici_Bike_Rio.js
+++ Tembici_Bike_Rio.js
@@ -38,9 +38,10 @@ esl_server.on('connection::open', (conn, id) => {
 esl_server.on('connection::close', (conn, id) => {
     console.log('Chamada com ID: ' + id + ' encerrada')
     ativa.find((element, index) => {
-        if(element[0] = id){
-            ativa.splice(index)
-        }
+        console.log(element)
+        //if(element[0] = id){
+        //    ativa.splice(index)
+        //}
     })
 })
 
---
+++ teste.js
@@ -0,0 +1,5 @@
+let lista = [[1, 2], 2, 3, 5, 6, 7, 8, 9, 0]
+
+lista.find((element, index) => {
+  console.log(element[0])
+})
