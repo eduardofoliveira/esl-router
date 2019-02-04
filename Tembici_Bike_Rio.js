abaaa02b7433cd274bc5b773b314d98338ba4802
Eduardo <eduardo_felipe_oliveira@yahoo.com.br>
2019-02-04 13:49:52 -0200

    Tudo para espera

--- Tembici_Bike_Rio.js
+++ Tembici_Bike_Rio.js
@@ -4,7 +4,7 @@ var esl_server = new esl.Server({host: '0.0.0.0', port: 8085, myevents:true}, fu
     console.log("Gerenciador de fila iniciado")
 })
 
-let tempo = 20000
+let tempo = 30000
 const lista = []
 
 esl_server.on('connection::ready', function(conn, id) {
@@ -18,17 +18,19 @@ esl_server.on('connection::ready', function(conn, id) {
 })
 
 setInterval(() => {
-    let conn = lista.shift()
+    if(lista.length > 0){
+        let conn = lista.shift()
 
-    conn.execute('set', 'effective_caller_id_name=${caller_id_number}Bike_Rio',  function(cb) {
-        conn.execute('set', 'effective_caller_id_number=${caller_id_number}Bike_Rio', function(cb) {
-            conn.execute('set', 'bridge_generate_comfort_noise=true', function(cb) {
-                conn.execute('bridge', `sofia/gateway/gateway_cloud/551140036054`, function(cb) {
-                    conn.execute('hangup', function(cb) {})
+        conn.execute('set', 'effective_caller_id_name=${caller_id_number}Bike_Rio',  function(cb) {
+            conn.execute('set', 'effective_caller_id_number=${caller_id_number}Bike_Rio', function(cb) {
+                conn.execute('set', 'bridge_generate_comfort_noise=true', function(cb) {
+                    conn.execute('bridge', `sofia/gateway/gateway_cloud/551140036054`, function(cb) {
+                        conn.execute('hangup', function(cb) {})
+                    })
                 })
             })
         })
-    })
+    }
 }, tempo)
 
 esl_server.on('connection::open', (conn, id) => {
