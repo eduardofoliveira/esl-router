65e3adf0b831d98b0f6d3e1c7512659f79b90aad
Eduardo <eduardo_felipe_oliveira@yahoo.com.br>
2019-02-14 11:19:51 -0200

    Remoção de mensagem

--- esl.js
+++ esl.js
@@ -1,32 +1,61 @@
 const esl = require('esl')
 
-let chamadas = []
 let chamadas_ativas = []
+let chamadas_na_ura = []
+let chamadas_na_espera = []
+let chamadas_no_basix = []
+
 let limit = 18
 let tempo = 20000
+
+const removerListaUra = (id) => {
+  for (let index = 0; index < chamadas_na_ura.length; index++) {
+    if (chamadas_na_ura[index] === id) {
+      chamadas_na_ura.splice(index, 1)
+    }
+  }
+}
+
+const removerListaEspera = (id) => {
+  for (let index = 0; index < chamadas_na_espera.length; index++) {
+    if (chamadas_na_espera[index] === id) {
+      chamadas_na_espera.splice(index, 1)
+    }
+  }
+}
 
 const call_handler = async function() {
 
   let id = this.uuid
   let from = this.data['Caller-Caller-ID-Number']
   let to = this.data['Channel-Destination-Number']
-  console.log(`chamada inicou ${id}`)
   chamadas_ativas.push(id)
 
   try {
     this.onceAsync('CHANNEL_HANGUP').then(function(){
       let id = this.uuid
-      console.log(`chamada terminada ${id}`)
-      
-      for (let index = 0; index < chamadas.length; index++) {
-        if (chamadas[index][1] === id) {
-          chamadas.splice(index, 1)
-        }
-      }
     
       for (let index = 0; index < chamadas_ativas.length; index++) {
         if (chamadas_ativas[index] === id) {
           chamadas_ativas.splice(index, 1)
+        }
+      }
+
+      for (let index = 0; index < chamadas_na_ura.length; index++) {
+        if (chamadas_na_ura[index] === id) {
+          chamadas_na_ura.splice(index, 1)
+        }
+      }
+
+      for (let index = 0; index < chamadas_na_espera.length; index++) {
+        if (chamadas_na_espera[index][1] === id) {
+          chamadas_na_espera.splice(index, 1)
+        }
+      }
+
+      for (let index = 0; index < chamadas_no_basix.length; index++) {
+        if (chamadas_no_basix[index] === id) {
+          chamadas_no_basix.splice(index, 1)
         }
       }
     })
@@ -34,74 +63,59 @@ const call_handler = async function() {
     if (to === '40030374') {
       to = `5511${to}`
       from = `${from}Bike_Belem`
+      chamadas_na_ura.push(id)
 
       await this.command('answer')
       await this.command('playback', 'silence_stream://1000')
       await this.command('playback', '/home/ec2/tembici/IVR_4003_sem_auto_atendimento.wav')
-
-      if (chamadas_ativas.length > limit) {
-        await this.command('playback', '/home/ec2/tembici/Tembici_Ocupados.wav')
-        await this.hangup()
-      }else{
-        this.command('playback', 'local_stream://default')
-        chamadas.push([this, id, from, to])
-      }
+      this.command('playback', 'local_stream://default')
+      removerListaUra(id)
+      chamadas_na_espera.push([this, id, from, to])
     }
 
     if (to === '40030387') {
       to = `5511${to}`
       from = `${from}ManoBike`
+      chamadas_na_ura.push(id)
 
       await this.command('answer')
       await this.command('playback', 'silence_stream://1000')
       await this.command('playback', '/home/ec2/tembici/IVR_4003_sem_auto_atendimento.wav')
-
-      if (chamadas_ativas.length > limit) {
-        await this.command('playback', '/home/ec2/tembici/Tembici_Ocupados.wav')
-        await this.hangup()
-      }else{
-        this.command('playback', 'local_stream://default')
-        chamadas.push([this, id, from, to])
-      }
+      this.command('playback', 'local_stream://default')
+      removerListaUra(id)
+      chamadas_na_espera.push([this, id, from, to])
     }
 
     if (to === '40030391') {
       to = `5511${to}`
       from = `${from}Danoninho`
+      chamadas_na_ura.push(id)
 
       await this.command('answer')
       await this.command('playback', 'silence_stream://1000')
       await this.command('playback', '/home/ec2/tembici/IVR_4003_sem_auto_atendimento.wav')
-
-      if (chamadas_ativas.length > limit) {
-        await this.command('playback', '/home/ec2/tembici/Tembici_Ocupados.wav')
-        await this.hangup()
-      }else{
-        this.command('playback', 'local_stream://default')
-        chamadas.push([this, id, from, to])
-      }
+      this.command('playback', 'local_stream://default')
+      removerListaUra(id)
+      chamadas_na_espera.push([this, id, from, to])
     }
 
     if (to === '40036052') {
       to = `5511${to}`
       from = `${from}Bike_POA`
+      chamadas_na_ura.push(id)
 
       await this.command('answer')
       await this.command('playback', 'silence_stream://1000')
       await this.command('playback', '/home/ec2/tembici/IVR_Poa2018.wav')
-
-      if (chamadas_ativas.length > limit) {
-        await this.command('playback', '/home/ec2/tembici/Tembici_Ocupados.wav')
-        await this.hangup()
-      }else{
-        this.command('playback', 'local_stream://default')
-        chamadas.push([this, id, from, to])
-      }
+      this.command('playback', 'local_stream://default')
+      removerListaUra(id)
+      chamadas_na_espera.push([this, id, from, to])
     }
 
     if (to === '40036054') {
       to = `5511${to}`
       from = `${from}Bike_Rio`
+      chamadas_na_ura.push(id)
 
       await this.command('answer')
       await this.command('playback', 'silence_stream://1000')
@@ -112,38 +126,31 @@ const call_handler = async function() {
       })
       await this.command('play_and_get_digits', '1 1 1 5000 1 /home/ec2/tembici/IVR_Rio2018.wav')
 
-      if (chamadas_ativas.length > limit) {
-        await this.command('playback', '/home/ec2/tembici/Tembici_Ocupados.wav')
-        await this.hangup()
-      }else{
-        if(opcao === '1'){
-          to = '551100000005'
-        }
-
-        this.command('playback', 'local_stream://default')
-        chamadas.push([this, id, from, to])
+      if(opcao === '1'){
+        to = '551100000005'
       }
+
+      this.command('playback', 'local_stream://default')
+      removerListaUra(id)
+      chamadas_na_espera.push([this, id, from, to])
     }
 
     if (to === '40036055') {
       to = `5511${to}`
       from = `${from}Bike_Sampa`
+      chamadas_na_ura.push(id)
 
       await this.command('answer')
       await this.command('playback', 'silence_stream://1000')
-
-      if (chamadas_ativas.length > limit) {
-        await this.command('playback', '/home/ec2/tembici/Tembici_Ocupados.wav')
-        await this.hangup()
-      }else{
-        this.command('playback', 'local_stream://default')
-        chamadas.push([this, id, from, to])
-      }
+      this.command('playback', 'local_stream://default')
+      removerListaUra(id)
+      chamadas_na_espera.push([this, id, from, to])
     }
 
     if (to === '40036056') {
       to = `5511${to}`
       from = `${from}Bike_PE`
+      chamadas_na_ura.push(id)
 
       await this.command('answer')
       await this.command('playback', 'silence_stream://1000')
@@ -154,49 +161,42 @@ const call_handler = async function() {
       })
       await this.command('play_and_get_digits', '1 1 1 5000 1 /home/ec2/tembici/IVR_PE2018.wav')
 
-      if (chamadas_ativas.length > limit) {
-        await this.command('playback', '/home/ec2/tembici/Tembici_Ocupados.wav')
-        await this.hangup()
-      }else{
-        if(opcao === '1'){
-          to = '551100000005'
-        }
-
-        this.command('playback', 'local_stream://default')
-        chamadas.push([this, id, from, to])
+      if(opcao === '1'){
+        to = '551100000005'
       }
+
+      this.command('playback', 'local_stream://default')
+      removerListaUra(id)
+      chamadas_na_espera.push([this, id, from, to])
     }
 
     if (to === '40039892') {
       to = `5511${to}`
       from = `${from}Bike_Salvador`
+      chamadas_na_ura.push(id)
 
       await this.command('answer')
       await this.command('playback', 'silence_stream://1000')
       await this.command('playback', '/home/ec2/tembici/IVR_Salvador2018.wav')
-
-      if (chamadas_ativas.length > limit) {
-        await this.command('playback', '/home/ec2/tembici/Tembici_Ocupados.wav')
-        await this.hangup()
-      }else{
-        this.command('playback', 'local_stream://default')
-        chamadas.push([this, id, from, to])
-      }
+      this.command('playback', 'local_stream://default')
+      removerListaUra(id)
+      chamadas_na_espera.push([this, id, from, to])
     }
 
-  // Segunda a Sexta
-  //05:00 IVR_0300_2420
-  //09:00 IVR_0300_2420_ibike
-  //18:00 IVR_0300_2420
-  //00:00 IVR_Noturno_Tembici
+    // Segunda a Sexta
+    //05:00 IVR_0300_2420
+    //09:00 IVR_0300_2420_ibike
+    //18:00 IVR_0300_2420
+    //00:00 IVR_Noturno_Tembici
 
-  // Sabado e Domingo
-  //05:00 IVR_0300_2420
-  //00:00 IVR_Noturno_Tembici
+    // Sabado e Domingo
+    //05:00 IVR_0300_2420
+    //00:00 IVR_Noturno_Tembici
 
     if (to === '2420') {
       to = `550300313${to}`
       from = `${from}Tembici`
+      chamadas_na_ura.push(id)
 
       await this.command('answer')
       await this.command('playback', 'silence_stream://1000')
@@ -212,84 +212,82 @@ const call_handler = async function() {
         await this.command('play_and_get_digits', '1 1 1 5000 1 /home/ec2/tembici/IVR_Inicio_0300.wav')
       }
       
-      if (chamadas_ativas.length > limit) {
-        await this.command('playback', '/home/ec2/tembici/Tembici_Ocupados.wav')
-        await this.hangup()
-      }else{
-        if(opcao === '1'){
-          to = '551100000006'
-        }
-        if(opcao === '3'){
-          to = '551100000007'
-        }
-        this.command('playback', 'local_stream://default')
-        chamadas.push([this, id, from, to])
+      if(opcao === '1'){
+        to = '551100000006'
       }
+      if(opcao === '3'){
+        to = '551100000007'
+      }
+      this.command('playback', 'local_stream://default')
+      removerListaUra(id)
+      chamadas_na_espera.push([this, id, from, to])
     }
 
   } catch (error) {
     console.log(`chamada terminada ${id}`)
     
-    for (let index = 0; index < chamadas.length; index++) {
-      if (chamadas[index][1] === id) {
-        chamadas.splice(index, 1)
-      }
-    }
-  
     for (let index = 0; index < chamadas_ativas.length; index++) {
       if (chamadas_ativas[index] === id) {
         chamadas_ativas.splice(index, 1)
       }
     }
+
+    for (let index = 0; index < chamadas_na_ura.length; index++) {
+      if (chamadas_na_ura[index] === id) {
+        chamadas_na_ura.splice(index, 1)
+      }
+    }
+
+    for (let index = 0; index < chamadas_na_espera.length; index++) {
+      if (chamadas_na_espera[index][1] === id) {
+        chamadas_na_espera.splice(index, 1)
+      }
+    }
+
+    for (let index = 0; index < chamadas_no_basix.length; index++) {
+      if (chamadas_no_basix[index] === id) {
+        chamadas_no_basix.splice(index, 1)
+      }
+    }
   }
-
-  //let opcao = ''
-
-  //this.on('DTMF', call => {
-  //  opcao += call.body['DTMF-Digit']
-  //})
-
-  //while(!(opcao === '1' || opcao === '2')){
-  //  if(opcao !== ''){
-  //    // opção inválida
-  //    //await this.command('playback', '/usr/share/freeswitch/sounds/en/us/callie/ivr/8000/ivr-pin_or_extension_is-invalid.wav')
-  //  }
-  //  opcao = ''
-  //  await this.command('play_and_get_digits', '1 1 1 3000 1 local_stream://moh')
-  //}
-  
-  //if(opcao === '1'){
-  //  await this.command('bridge', `sofia/gateway/Tfreeswitch/37115000`)
-  //  await this.hangup()
-  //}
-  //if(opcao === '2'){
-  //  await this.command('bridge', `sofia/gateway/Tfreeswitch/35880866`)
-  //  await this.hangup()
-  //}
 }
 
 setInterval(async () => {
-  if (chamadas.length > 0) {
-    let [conn, id, from, to] = chamadas.shift()
+  if (chamadas_na_espera.length > 0 && chamadas_no_basix < limit) {
+    let [conn, id, from, to] = chamadas_na_espera.shift()
 
     try {
       await conn.command('set', `effective_caller_id_name=${from}`)
       await conn.command('set', `effective_caller_id_number=${from}`)
       await conn.command('set', `bridge_generate_comfort_noise=true`)
+      removerListaEspera(id)
+      chamadas_no_basix.push(id)
       await conn.command('bridge', `sofia/gateway/gateway_cloud/${to}`)
       await conn.hangup()
     } catch (error) {
       console.log(`chamada terminada ${id}`)
       
-      for (let index = 0; index < chamadas.length; index++) {
-        if (chamadas[index][1] === id) {
-          chamadas.splice(index, 1)
-        }
-      }
-    
       for (let index = 0; index < chamadas_ativas.length; index++) {
         if (chamadas_ativas[index] === id) {
           chamadas_ativas.splice(index, 1)
+        }
+      }
+  
+      for (let index = 0; index < chamadas_na_ura.length; index++) {
+        if (chamadas_na_ura[index] === id) {
+          chamadas_na_ura.splice(index, 1)
+        }
+      }
+  
+      for (let index = 0; index < chamadas_na_espera.length; index++) {
+        if (chamadas_na_espera[index][1] === id) {
+          chamadas_na_espera.splice(index, 1)
+        }
+      }
+  
+      for (let index = 0; index < chamadas_no_basix.length; index++) {
+        if (chamadas_no_basix[index] === id) {
+          chamadas_no_basix.splice(index, 1)
         }
       }
     }
@@ -297,8 +295,10 @@ setInterval(async () => {
 }, tempo)
 
 setInterval(async () => {
-  console.log(`quantidade de chamadas ativas: ${chamadas_ativas.length}`)
-  console.log(`quantidade de chamadas na espera: ${chamadas.length}`)
+  console.log(`Chamadas ativas: ${chamadas_ativas.length}`)
+  console.log(`Chamadas na ura: ${chamadas_na_ura.length}`)
+  console.log(`Chamadas na espera: ${chamadas_na_espera.length}`)
+  console.log(`Chamadas no basix: ${chamadas_no_basix.length}`)
 }, 5000)
 
 const server = esl.server(call_handler)
